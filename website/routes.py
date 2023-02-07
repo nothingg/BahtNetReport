2a0387aca05ad19e79bca3f70c402667f5a7c27b
Bird-GHB <>
2023-02-07 16:21:10 +0700

    routes - create request parameter

--- website/routes.py
+++ website/routes.py
@@ -25,8 +25,26 @@ def form_page(id_data):
 def update():
     if request.method == 'POST':
         item = Reports.query.get(request.form['cs_ref'])
+        # item.instruction_id = request.form['instruction_id']
+        # item.mt = request.form['mt']
+        # item.ctgypurp = request.form['ctgypurp']
+        # item.dr_bic = request.form['dr_bic']
+        # item.dr_acct = request.form['dr_acct']
+        # item.cr_bic = request.form['cr_bic']
+        # item.cr_acct = request.form['cr_acct']
+        # item.dr_amt = request.form['dr_amt']
+        # item.cr_amt = request.form['cr_amt']
+        # item.status = request.form['status']
+        # item.error = request.form['error']
+        # item.time = request.form['time']
+        # item.ch = request.form['ch']
+        # item.transmission_type = request.form['transmission_type']
+        # item.debtor_acct = request.form['debtor_acct']
         item.debtor_name = request.form['debtor_name']
+        # item.creditor_acct = request.form['creditor_acct']
         item.creditor_name = request.form['creditor_name']
+        # item.dept = request.form['dept']
+        # item. = request.form['']
 
         db.session.commit()
         return redirect(url_for('routes.list_data'))
